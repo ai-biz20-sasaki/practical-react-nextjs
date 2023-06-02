76379ea642a571d5db20ec3124dcd6d123a8d183
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-02 10:44:36 +0900

    improved comments

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,5 +1,5 @@
 //無名関数でコンポーネントを定義し、Textという変数に代入する
-//Textは親から'content'というデータを受け取る
+//子コンポーネントTextは親コンポーネントMessageから'content'というデータを受け取る
 const Text = (props: { content: string }) => {
   //propsからcontentをいう値を取り出す
   const { content } = props
@@ -8,12 +8,13 @@ const Text = (props: { content: string }) => {
 }
 
 //無名関数でコンポーネントを定義し、Messageという変数に代入する
+//Messageは親コンポーネント
 const Message = (props: {}) => {
   const content1 = 'This is parent component'
   const content2 = 'Message uses Text component'
   return (
     <div>
-      {/* contentというキーでコンポーネントにデータを渡す */}
+      {/* contentというキーで子コンポーネントにデータを渡す */}
       <Text content={ content1 } />
       {/* 違うデータを渡すと、違う内容が表示される */}
       <Text content={ content2 } />
